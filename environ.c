5101509892aaf8cae68dfc356f59c21609eab371
amedzaphilips <94114893+amedzaphilips@users.noreply.github.com>
2023-10-19 19:46:17 +0000

    Update environ.c

--- environ.c
+++ environ.c
@@ -6,9 +6,9 @@
  *          constant function prototype.
  * Return: Always 0
  */
-int _myenv(info_t *info)
+int _myenv(info_in *data)
 {
-	print_list_str(info->env);
+	print_list_str(data->env);
 	return (0);
 }
 
@@ -19,9 +19,9 @@ int _myenv(info_t *info)
  *
  * Return: the value
  */
-char *_getenv(info_t *info, const char *name)
+char *_getenv(info_in *data, const char *name)
 {
-	list_t *node = info->env;
+	list_t *node = data->env;
 	char *p;
 
 	while (node)
@@ -41,14 +41,14 @@ char *_getenv(info_t *info, const char *name)
  *        constant function prototype.
  *  Return: Always 0
  */
-int _mysetenv(info_t *info)
+int _mysetenv(info_in *data)
 {
-	if (info->argc != 3)
+	if (data->agc != 3)
 	{
 		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (_setenv(info, info->argv[1], info->argv[2]))
+	if (_setenv(data, data->agv[1], data->agv[2]))
 		return (0);
 	return (1);
 }
@@ -59,17 +59,17 @@ int _mysetenv(info_t *info)
  *        constant function prototype.
  *  Return: Always 0
  */
-int _myunsetenv(info_t *info)
+int _myunsetenv(info_in *data)
 {
 	int i;
 
-	if (info->argc == 1)
+	if (data->agc == 1)
 	{
 		_eputs("Too few arguements.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->argc; i++)
-		_unsetenv(info, info->argv[i]);
+	for (i = 1; i <= data->agc; i++)
+		_unsetenv(data, data->agv[i]);
 
 	return (0);
 }
@@ -80,13 +80,13 @@ int _myunsetenv(info_t *info)
  *          constant function prototype.
  * Return: Always 0
  */
-int populate_env_list(info_t *info)
+int populate_env_list(info_in *data)
 {
 	list_t *node = NULL;
 	size_t i;
 
 	for (i = 0; environ[i]; i++)
 		add_node_end(&node, environ[i], 0);
-	info->env = node;
+	data->env = node;
 	return (0);
 }

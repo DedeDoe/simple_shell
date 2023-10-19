0cf65f3bdef9503259d7a3495d0c946cfe004c4e
amedzaphilips <danielamedzaphilips@gmail.com>
2023-10-19 10:05:42 -0700

    redo

---
+++ getinfo.c
@@ -0,0 +1,84 @@
+#include "shell.h"
+
+/**
+ * clear_info - Initializes fields of the info_t struct to NULL.
+ * @info: Pointer to the info_t struct to be cleared.
+ *
+ * This function sets the fields arg, argv, path, and argc of the info_t struct
+ * pointed to by 'info' to NULL.
+ */
+void clear_info(info_t *info)
+{
+	info->arg = NULL;
+	info->argv = NULL;
+	info->path = NULL;
+	info->argc = 0;
+}
+
+/**
+ * set_info - Initializes fields of the info_t struct.
+ * @info: Pointer to the info_t struct to be set.
+ * @av: Argument vector from the main function.
+ *
+ * This fxn initializes the fields fname, argv, path, argc, and other fields
+ * of the info_t struct based on given argument vector 'av' and 'arg' field
+ * in the info_t struct.
+ */
+void set_info(info_t *info, char **av)
+{
+	int i = 0;
+
+	info->fname = av[0];
+	if (info->arg)
+	{
+		info->argv = strtow(info->arg, " \t");
+		if (!info->argv)
+		{
+			info->argv = malloc(sizeof(char *) * 2);
+			if (info->argv)
+			{
+				info->argv[0] = _strdup(info->arg);
+				info->argv[1] = NULL;
+			}
+		}
+		for (i = 0; info->argv && info->argv[i]; i++)
+			;
+		info->argc = i;
+
+		replace_alias(info);
+		replace_vars(info);
+	}
+}
+
+/**
+ * free_info - Frees allocated memory in the info_t struct.
+ * @info: Pointer to the info_t struct.
+ * @all: 1 if all fields need to be freed, 0 otherwise.
+ *
+ * This fxn release memory associated with various fields of info_t struct,
+ * such as argv, arg, env, history, alias, cmd_buf, environ, and readfd.
+ * 'all' parameter tells whether to free all fields or just specific ones.
+ */
+void free_info(info_t *info, int all)
+{
+	ffree(info->argv);
+	info->argv = NULL;
+	info->path = NULL;
+	if (all)
+	{
+		if (!info->cmd_buf)
+			free(info->arg);
+		if (info->env)
+			free_list(&(info->env));
+		if (info->history)
+			free_list(&(info->history));
+		if (info->alias)
+			free_list(&(info->alias));
+		ffree(info->environ);
+			info->environ = NULL;
+		bfree((void **)info->cmd_buf);
+		if (info->readfd > 2)
+			close(info->readfd);
+		_putchar(BUF_FLUSH);
+	}
+}
---
+++ shell_loop.c
@@ -0,0 +1,172 @@
+#include "shell.h"
+
+/**
+ * hsh - Main shell loop.
+ * @info: Pointer to the info_t struct.
+ * @av: The argument vector from main().
+ *
+ * This function implements the main loop of the shell, where it reads input,
+ * processes built-in commands, and executes external commands.
+ *
+ * Return: Returns 0 on success, 1 on error, or the appropriate exit code.
+ */
+int hsh(info_t *info, char **av)
+{
+	ssize_t r = 0;
+	int builtin_ret = 0;
+
+	while (r != -1 && builtin_ret != -2)
+	{
+		clear_info(info);
+		if (interactive(info))
+			_puts("$ ");
+		_eputchar(BUF_FLUSH);
+		r = get_input(info);
+		if (r != -1)
+		{
+			set_info(info, av);
+			builtin_ret = find_builtin(info);
+			if (builtin_ret == -1)
+				find_cmd(info);
+		}
+		else if (interactive(info))
+			_putchar('\n');
+		free_info(info, 0);
+	}
+	write_history(info);
+	free_info(info, 1);
+	if (!interactive(info) && info->status)
+		exit(info->status);
+	if (builtin_ret == -2)
+	{
+		if (info->err_num == -1)
+			exit(info->status);
+		exit(info->err_num);
+	}
+	return (builtin_ret);
+}
+
+/**
+ * find_builtin - Finds and executes a built-in command.
+ * @info: Pointer to the info_t struct.
+ *
+ * This function looks for the given cmd in the list of built-in commands,
+ * executes the command if found, and returns the result.
+ *
+ * Return: Returns -1 if the built-in is not found,
+ *         0 if the built-in executed successfully,
+ *         1 if the built-in was found but not successful,
+ *         2 if the built-in signals an exit().
+ */
+int find_builtin(info_t *info)
+{
+	int i, built_in_ret = -1;
+	builtin_table builtintbl[] = {
+		{"exit", _myexit},
+		{"env", _myenv},
+		{"help", _myhelp},
+		{"history", _myhistory},
+		{"setenv", _mysetenv},
+		{"unsetenv", _myunsetenv},
+		{"cd", _mycd},
+		{"alias", _myalias},
+		{NULL, NULL}
+	};
+
+	for (i = 0; builtintbl[i].type; i++)
+		if (_strcmp(info->argv[0], builtintbl[i].type) == 0)
+		{
+			info->line_count++;
+			built_in_ret = builtintbl[i].func(info);
+			break;
+		}
+	return (built_in_ret);
+}
+
+/**
+ * find_cmd - Finds and executes a command in the PATH.
+ * @info: Pointer to the info_t struct.
+ *
+ * This function searches for the provided command in the directories specified
+ * in the PATH environment variable and executes the command if found.
+ *
+ * Return: void
+ */
+void find_cmd(info_t *info)
+{
+	char *path = NULL;
+	int i, k;
+
+	info->path = info->argv[0];
+	if (info->linecount_flag == 1)
+	{
+		info->line_count++;
+		info->linecount_flag = 0;
+	}
+	for (i = 0, k = 0; info->arg[i]; i++)
+		if (!is_delim(info->arg[i], " \t\n"))
+			k++;
+	if (!k)
+		return;
+
+	path = find_path(info, _getenv(info, "PATH="), info->argv[0]);
+	if (path)
+	{
+		info->path = path;
+		fork_cmd(info);
+	}
+	else
+	{
+		if ((interactive(info) || _getenv(info, "PATH=")
+					|| info->argv[0][0] == '/') && is_cmd(info, info->argv[0]))
+			fork_cmd(info);
+		else if (*(info->arg) != '\n')
+		{
+			info->status = 127;
+			print_error(info, "not found\n");
+		}
+	}
+}
+
+/**
+ * fork_cmd - Forks a child process to execute a command.
+ * @info: Pointer to the info_t struct.
+ *
+ * This fxn forks a child process to execute an external cmd using execve().
+ * It waits for the child process to complete and updates the exit status.
+ *
+ * Return: void
+ */
+void fork_cmd(info_t *info)
+{
+	pid_t child_pid;
+
+	child_pid = fork();
+	if (child_pid == -1)
+	{
+		/* TODO: PUT ERROR FUNCTION */
+		perror("Error:");
+		return;
+	}
+	if (child_pid == 0)
+	{
+		if (execve(info->path, info->argv, get_environ(info)) == -1)
+		{
+			free_info(info, 1);
+			if (errno == EACCES)
+				exit(126);
+			exit(1);
+		}
+		/* TODO: PUT ERROR FUNCTION */
+	}
+	else
+	{
+		wait(&(info->status));
+		if (WIFEXITED(info->status))
+		{
+			info->status = WEXITSTATUS(info->status);
+			if (info->status == 126)
+				print_error(info, "Permission denied\n");
+		}
+	}
+}

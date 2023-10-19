18b5c6b812744ff4a368675e63313bf7c4d17c72
amedzaphilips <94114893+amedzaphilips@users.noreply.github.com>
2023-10-19 19:00:09 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,44 +1,44 @@
 #include "shell.h"
 
 /**
- * main - entry point
- * @ac: arg count
- * @av: arg vector
+ * main - entry point of the shell
+ * @agc: argument count
+ * @agv: argument vector
  *
- * Return: 0 on success, 1 on error
+ * Return: 0 on success, and 1 on error
  */
-int main(int ac, char **av)
+int main(int agc, char **agv)
 {
+	int eg = 2;
 	info_t info[] = { INFO_INIT };
-	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
 		"add $3, %0"
-		: "=r" (fd)
-		: "r" (fd));
+		: "=r" (eg)
+		: "r" (eg));
 
-	if (ac == 2)
+	if (agc == 2)
 	{
-		fd = open(av[1], O_RDONLY);
-		if (fd == -1)
+		eg = open(argv[1], O_RDONLY);
+		if (eg == -1)
 		{
 			if (errno == EACCES)
-				exit(126);
+				exit(101);
 			if (errno == ENOENT)
 			{
-				_eputs(av[0]);
+				_eputs(agv[0]);
 				_eputs(": 0: Can't open ");
-				_eputs(av[1]);
+				_eputs(agv[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
-				exit(127);
+				exit(102);
 			}
-			return (EXIT_FAILURE);
+			return (1);
 		}
-		info->readfd = fd;
+		info->readeg = eg;
 	}
 	populate_env_list(info);
 	read_history(info);
-	hsh(info, av);
-	return (EXIT_SUCCESS);
+	hsh(info, agv);
+	return (0);
 }

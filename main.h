b980dfbcf195e450f8598771f798070315d0838a
amedzaphilips <94114893+amedzaphilips@users.noreply.github.com>
2023-10-19 15:21:06 +0000

    Update main.h

--- main.h
+++ main.h
@@ -10,17 +10,11 @@
 #include <stdarg.h>
 
 int _putchar(char c);
-int my_print(char *str);
-char *address(char *path, char *arg);
-char *getpath(char *arg);
-void exit_cmd(void);
-void execute_env(void);
 size_t custom_strlen(char *str);
 char *custom_strdup(char *original);
 char *custom_strcat(char *destination, char *source);
 int custom_strcmp(char *stra, char *strb);
 char *custom_strcpy(char *destination, char *source);
 void custom_print(char *string);
-void child(void);
 
 #endif
